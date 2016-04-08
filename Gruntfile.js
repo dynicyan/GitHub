2ef58fa190c394ec93bf16cf09e0fa9bb3d6cf77
dynicyan <dynicyan@qq.com>
2016-04-08 17:48:56 +0800

    gr

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,7 +89,7 @@ module.exports = function(grunt) {
                 options: {
                     pretty: true,
                         data: function(dest, src) {
-                        return require('./slideimg/_source/data/connect.json');
+                        return require('slideimg/_source/data/connect.json');
                     }
                 },
                 files: [{
